d1f8a4cd2f64f7ff178021cd20a35c0f8831ee93
nghiafl1 <nghiafl04@gmail.com>
2024-10-10 22:33:30 +0700

    cap nhat vien dan cho xe tang

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,12 @@ public class Player : MonoBehaviour
     private bool IsFacingRight = true;
     private Rigidbody2D Rb;
     private Animator animator;
+    public GameObject Bullet;
+    public GameObject Bullet2;
+    public Transform FirePos;
+    public float SpeedFire = 1f;
+    public float BulletForce;
+    private float speedFire;
     void Start()
     {
         Rb = GetComponent<Rigidbody2D>();
@@ -17,10 +23,16 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Trai_phai = Input.GetAxis("Horizontal");
+        Trai_phai = Input.GetAxisRaw("Horizontal");
+        //if ((transform.position.x < -8.73 && Trai_phai < 0) || (transform.position.x > 8.73 && Trai_phai > 0)) return;
         Rb.velocity = new Vector2(Trai_phai * Speed, Rb.velocity.y);
         Flip();
         animator.SetFloat("move", Mathf.Abs(Trai_phai));
+        speedFire -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && speedFire < 0) 
+        {
+            FireBullet();
+        }
     }
     void Flip()
     {
@@ -30,6 +42,22 @@ public class Player : MonoBehaviour
             Vector3 Kich_thuoc = transform.localScale;
             Kich_thuoc.x *= -1;
             transform.localScale = Kich_thuoc;
+        }
+    }
+    void FireBullet()
+    {
+        speedFire = SpeedFire;
+        if (IsFacingRight == true)
+        {           
+            GameObject bullet = Instantiate(Bullet, FirePos.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.right * BulletForce, ForceMode2D.Impulse);
+        }
+        else
+        { 
+            GameObject bullet = Instantiate(Bullet2, FirePos.position, Quaternion.identity);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(-transform.right * BulletForce, ForceMode2D.Impulse);
         }
     }
 }
